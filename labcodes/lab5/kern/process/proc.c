02561ddbaf976f906f63982d076536ce1e8371d1
ProKil <prokilchu@gmail.com>
2018-03-21 22:42:21 +0800

    add lab1/2/3/4

--- labcodes/lab5/kern/process/proc.c
+++ labcodes/lab5/kern/process/proc.c
@@ -109,6 +109,20 @@ alloc_proc(void) {
      *       uint32_t wait_state;                        // waiting state
      *       struct proc_struct *cptr, *yptr, *optr;     // relations between processes
 	 */
+        // special members
+        proc->state = PROC_UNINIT;                      // Process state
+        proc->pid = -1;                   // Process ID
+        proc->cr3 = boot_cr3;                              // CR3 register: the base addr of Page Directroy Table(PDT)
+        proc->parent = current;                 // the parent process
+        // others clear to 0
+        proc->runs = 0;                                   // the running times of Proces
+        proc->kstack = 0;                           // Process kernel stack
+        proc->need_resched = 0;                 // bool value: need to be rescheduled to release CPU?
+        proc->mm = NULL;                       // Process's memory management field
+        memset(&(proc->context), 0, sizeof(struct context));                     // Switch here to run process
+        proc->tf = NULL;                       // Trap frame for current interrupt
+        proc->flags = 0;                             // Process flag
+        memset(proc->name, 0, PROC_NAME_LEN + 1);               // Process name 
     }
     return proc;
 }
@@ -395,6 +409,24 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    5. insert proc_struct into hash_list && proc_list
     //    6. call wakeup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
+    if (!(proc = alloc_proc())) {
+        goto fork_out;
+    }
+
+    if (setup_kstack(proc)) {
+        goto bad_fork_cleanup_proc;
+    }
+    if (copy_mm(clone_flags, proc)) {
+        goto bad_fork_cleanup_kstack;
+    }
+    copy_thread(proc, stack, tf);
+
+    proc->pid = get_pid();
+    hash_proc(proc);
+    list_add(&proc_list, &(proc->list_link));
+    nr_process ++;
+    wakeup_proc(proc);
+    ret = proc->pid;
 
 	//LAB5 YOUR CODE : (update LAB4 steps)
    /* Some Functions
